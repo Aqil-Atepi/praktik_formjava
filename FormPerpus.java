d73afff890b28c6a724251907038052329f888e7
Aqil-Atepi <167964589+Aqil-Atepi@users.noreply.github.com>
2025-05-10 19:13:03 +0700

    Add files via upload

---
+++ FormPerpus.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.formperpus;
+
+/**
+ *
+ * @author newpl
+ */
+public class FormPerpus {
+
+    public static void main(String[] args) {
+        registrasiForm reg = new registrasiForm();
+        reg.setVisible(true);
+        reg.pack();
+        reg.setLocationRelativeTo(null);
+        reg.setDefaultCloseOperation(registrasiForm.EXIT_ON_CLOSE);
+    }
+}
